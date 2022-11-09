5e116d3b3e1a3304189703f729869c150c025c27
Binh <ucvanbinh.tdc2020@gmail.com>
2022-11-09 16:11:49 +0700

    create project

--- app/src/main/java/tdc/edu/vn/apbansach/model/Books.java
+++ app/src/main/java/tdc/edu/vn/apbansach/model/Books.java
@@ -2,6 +2,16 @@ package tdc.edu.vn.apbansach.model;
 
 public class Books {
     private String BookName, BookPrice,  BookInfo;
+    String categories_id;
+
+    public String getCategories_id() {
+        return categories_id;
+    }
+
+    public void setCategories_id(String categories_id) {
+        this.categories_id = categories_id;
+    }
+
     int ImageBook;
     public Books() {
     }
