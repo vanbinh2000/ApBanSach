4c2be9ff7f391ead78e3bf555907b9c71b45aca5
quocthong2002 <20211tt1072@mail.tdc.edu.vn>
2022-11-14 12:25:04 +0700

    Login screen

--- app/src/main/java/tdc/edu/vn/apbansach/LoginActivity.java
+++ app/src/main/java/tdc/edu/vn/apbansach/LoginActivity.java
@@ -1,19 +1,36 @@
 package tdc.edu.vn.apbansach;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     TextView tvSignUp;
-
+    Button btnLogin;
+    EditText txtMail, txtPass;
+    FirebaseAuth mAuth;
+    ImageView ivShowHideLoginPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
         setControl();
         setEvent();
     }
@@ -27,9 +44,55 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+        ivShowHideLoginPass.setImageResource(R.drawable.hide);
+        ivShowHideLoginPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtPass.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    txtPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    ivShowHideLoginPass.setImageResource(R.drawable.hide);
+                    txtPass.setSelection(txtPass.getText().length());
+                }
+                else{
+                    txtPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    ivShowHideLoginPass.setImageResource(R.drawable.view);
+                    txtPass.setSelection(txtPass.getText().length());
+                }
+            }
+        });
+    }
+
+    private void login() {
+        String Email = txtMail.getText().toString().trim();
+        String Pass = txtPass.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(Email, Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Login successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void setControl() {
         tvSignUp = findViewById(R.id.tvSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtMail = findViewById(R.id.txtEmail);
+        txtPass = findViewById(R.id.txtPass);
+        ivShowHideLoginPass = findViewById(R.id.ivShowHideLoginPass);
     }
 }
