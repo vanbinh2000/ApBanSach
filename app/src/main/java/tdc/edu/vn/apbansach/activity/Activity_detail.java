a695335ee34ba27b634f8e556966ebaf6f936089
Binh <ucvanbinh.tdc2020@gmail.com>
2022-11-19 17:24:07 +0700

    update detail

--- app/src/main/java/tdc/edu/vn/apbansach/activity/Activity_detail.java
+++ app/src/main/java/tdc/edu/vn/apbansach/activity/Activity_detail.java
@@ -1,5 +1,6 @@
 package tdc.edu.vn.apbansach.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
@@ -16,40 +22,48 @@ import tdc.edu.vn.apbansach.R;
 import tdc.edu.vn.apbansach.model.Products;
 
 public class Activity_detail extends AppCompatActivity {
-    ImageView imageView_detail;
-    TextView txtname_detail;
-    TextView txtdescription_detail;
-    TextView txtprice_detail;
-    ImageView imageView_back;
+   private ImageView imageView_detail;
+   private TextView txtname_detail;
+   private TextView txtdescription_detail;
+   private TextView txtprice_detail;
+   private ImageView imageView_back;
+   private String productsID = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        loadDetail();
+        loadDetail(productsID);
         ActionBack();
     }
-    public void loadDetail()
+    public void loadDetail(String productsID)
     {
 
-        imageView_detail = findViewById(R.id.idimage_detail);
-        txtname_detail = findViewById(R.id.idname_detail);
-        txtdescription_detail = findViewById(R.id.idDescription_detail);
-        txtprice_detail = findViewById(R.id.idprice_detail);
-        Bundle bundle = getIntent().getExtras();
-        if (bundle == null)
-        {
-            return;
-        }
-        Products products = (Products) bundle.get("object_prodcts");
-        Picasso.get().load(products.getImage())
-                .fit()
-                .centerCrop()
-                .into(imageView_detail);
-        txtname_detail.setText(products.getProducts_name());
-        txtdescription_detail.setText(products.getDescription());
-        DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
 
-        txtprice_detail.setText(decimalFormat.format(Integer.valueOf(products.getPrice().trim()))+ " Đ");
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Products");
+        databaseReference.child("products_id").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    Products products = snapshot.getValue(Products.class);
+                    Picasso.get().load(products.getImage())
+                            .fit()
+                            .centerCrop()
+                            .into(imageView_detail);
+                    txtname_detail.setText(products.getProducts_name());
+                    txtdescription_detail.setText(products.getDescription());
+                    DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
+
+                    txtprice_detail.setText(decimalFormat.format(Integer.valueOf(products.getPrice().trim())) + " Đ");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
     public void ActionBack()
     {
